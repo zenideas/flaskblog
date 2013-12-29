a1669a471411a29c82b91a28f938b0ca3e9f9a32
NAZRUL ISLAM <nislam@ashrar-Dell-System-Vostro-3450.(none)>
2013-12-29 15:51:45 +0600

    First Commit

---
+++ application/controller.py
@@ -0,0 +1,105 @@
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from application import app, db, lm, oid
+from forms import LoginForm, UserForm
+from models import User, ROLE_USER, ROLE_ADMIN
+from datetime import datetime
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user = g.user
+    posts = [
+        {'author':{ 'nickname': 'John' }, 'body': 'Beautiful day in Portland!'},
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
+def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',title='Sign In', form = form, providers=app.config['OPENID_PROVIDERS'])
+
+
+@app.route('/user/<email>')
+@login_required
+def user(email):
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        flash("User " + email + "not exists")
+        redirect('index')
+    posts = [{"author" : user, "body" : "Test Post #1"},{"author" : user, "body" : "Test Post #2"}]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit', methods=['POST', 'GET'])
+@login_required
+def edit():
+    form = UserForm()
+    if request.method == 'POST' and form.validate_on_submit:
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('your changed has been saved')
+        redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edituser.html', form=form, user=g.user)
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
---
+++ application/models.py
@@ -0,0 +1,53 @@
+from application import db
+from hashlib import md5
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __init__(self, nickname, email, role=ROLE_USER):
+        self.nickname = nickname
+        self.email = email
+        self.role = role
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&' + str(size)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, body, timestamp, user_id):
+        self.body = body
+        self.timestamp = timestamp
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
